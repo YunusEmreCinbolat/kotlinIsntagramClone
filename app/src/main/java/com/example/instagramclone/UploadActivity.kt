feefb563be82dc40e4dbc1393ba4c79f94ecc102
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-09-19 20:00:43 +0300

    add read_external_storage

--- app/src/main/java/com/example/instagramclone/UploadActivity.kt
+++ app/src/main/java/com/example/instagramclone/UploadActivity.kt
@@ -1,21 +1,45 @@
 package com.example.instagramclone
 
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.MediaStore.Audio.Media
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.instagramclone.databinding.ActivityUploadBinding
+import com.google.android.material.snackbar.Snackbar
 
 class UploadActivity : AppCompatActivity() {
     private lateinit var binding: ActivityUploadBinding
+    private lateinit var activityResultLauncher:ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityUploadBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.toolbarUpload.title="Upload"
+        setSupportActionBar(binding.toolbarUpload)
     }
     fun upLoad(view: View){
 
     }
-    fun selectImage(view: View){}
+    fun selectImage(view: View){
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Snackbar.make(view,"İzin verecek misin",Snackbar.LENGTH_INDEFINITE).setAction("Give permision"){
+
+                }.show()
+            }
+            else{
+                val intenToGallery=Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+            }
+        }
+    }
 }
